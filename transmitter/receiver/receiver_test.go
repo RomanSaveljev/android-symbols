48d50e755bbad81a0682b10fdadd9220733f5bba
Roman Saveljev <roman.saveljev@haltian.com>
2015-09-22 12:54:02 +0300

    Optimized hex converter implementation

--- transmitter/receiver/receiver_test.go
+++ transmitter/receiver/receiver_test.go
@@ -3,20 +3,40 @@ package receiver
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"github.com/RomanSaveljev/android-symbols/receiver/src/lib"
-	"github.com/RomanSaveljev/android-symbols/transmitter/src/lib/mock"
+	"github.com/RomanSaveljev/android-symbols/transmitter/mock"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"testing"
 )
 
+func BenchmarkExtractRolling(b *testing.B) {
+	for i:= 0; i < b.N; i++ {
+		extractRolling("deadbeef")
+		extractRolling("12345678")
+		extractRolling("12ab34cd")
+		extractRolling("fff09ae3")
+	}
+}
+
+func BenchmarkSscanf(b *testing.B) {
+	for i:= 0; i < b.N; i++ {
+		var result uint32
+		fmt.Sscanf("deadbeef", "%x", &result)
+		fmt.Sscanf("12345678", "%x", &result)
+		fmt.Sscanf("12ab34cd", "%x", &result)
+		fmt.Sscanf("fff09ae3", "%x", &result)
+	}
+}
+
 func TestReceiver(t *testing.T) {
 	assert := assert.New(t)
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	client := mock_transmitter.NewMockClient(mockCtrl)
+	client := mock.NewMockClient(mockCtrl)
 	gomock.InOrder(
 		client.EXPECT().Call("Synchronizer.StartFile", "/a/b/c/d.txt", gomock.Any()).SetArg(2, "file-token"),
 		client.EXPECT().Call("file-token.StartStream", gomock.Not(nil), gomock.Any()).SetArg(2, "stream-token"),
@@ -39,13 +59,13 @@ func TestReceiverSaveChunk(t *testing.T) {
 	defer mockCtrl.Finish()
 
 	var chunk receiver.Chunk
-	chunk.Rolling = hex.EncodeToString([]byte{1, 2})
+	chunk.Rolling = "01020304"
 	chunk.Strong = hex.EncodeToString([]byte{3, 4})
 	n, err := rand.Read(chunk.Data[:])
 	assert.NoError(err)
 	assert.Equal(len(chunk.Data), n)
 
-	client := mock_transmitter.NewMockClient(mockCtrl)
+	client := mock.NewMockClient(mockCtrl)
 	gomock.InOrder(
 		client.EXPECT().Call("Synchronizer.StartFile", "/a/b/c/d.txt", gomock.Any()).SetArg(2, "tkn"),
 		client.EXPECT().Call("tkn.SaveChunk", chunk, gomock.Any()),
@@ -53,7 +73,7 @@ func TestReceiverSaveChunk(t *testing.T) {
 
 	rcv, err := NewReceiver("/a/b/c/d.txt", client)
 	assert.NoError(err)
-	err = rcv.SaveChunk([]byte{1, 2}, []byte{3, 4}, chunk.Data[:])
+	err = rcv.SaveChunk(0x01020304, []byte{3, 4}, chunk.Data[:])
 	assert.NoError(err)
 	err = rcv.Close()
 	assert.NoError(err)
@@ -68,7 +88,7 @@ func TestReceiverCollectSignatures(t *testing.T) {
 	two := receiver.Signature{Rolling: "1234", Strong: "4567"}
 	three := receiver.Signature{Rolling: "1234", Strong: "789a"}
 
-	client := mock_transmitter.NewMockClient(mockCtrl)
+	client := mock.NewMockClient(mockCtrl)
 	gomock.InOrder(
 		client.EXPECT().Call("Synchronizer.StartFile", "/a/b/c/d.txt", gomock.Any()).SetArg(2, "tkn"),
 		client.EXPECT().Call("tkn.NextSignature", gomock.Not(nil), gomock.Any()).SetArg(2, one),
@@ -81,9 +101,9 @@ func TestReceiverCollectSignatures(t *testing.T) {
 	assert.NoError(err)
 	sigs, err := rcv.Signatures()
 	assert.NoError(err)
-	assert.True(sigs.Get([]byte{0xab, 0xcd}).Has([]byte{0xde, 0xf0}))
-	assert.True(sigs.Get([]byte{0x12, 0x34}).Has([]byte{0x45, 0x67}))
-	assert.True(sigs.Get([]byte{0x12, 0x34}).Has([]byte{0x78, 0x9a}))
+	assert.True(sigs.Get(0xabcd).Has([]byte{0xde, 0xf0}))
+	assert.True(sigs.Get(0x1234).Has([]byte{0x45, 0x67}))
+	assert.True(sigs.Get(0x1234).Has([]byte{0x78, 0x9a}))
 	err = rcv.Close()
 	assert.NoError(err)
 }
@@ -93,7 +113,7 @@ func TestReceiverGetCachedSignatures(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	client := mock_transmitter.NewMockClient(mockCtrl)
+	client := mock.NewMockClient(mockCtrl)
 	gomock.InOrder(
 		client.EXPECT().Call("Synchronizer.StartFile", "/a/b/c/d.txt", gomock.Any()).SetArg(2, "tkn"),
 		client.EXPECT().Call("tkn.NextSignature", gomock.Not(nil), gomock.Any()).Return(io.EOF),
@@ -103,10 +123,10 @@ func TestReceiverGetCachedSignatures(t *testing.T) {
 	assert.NoError(err)
 	sigs, err := rcv.Signatures()
 	assert.NoError(err)
-	candidates := sigs.Get([]byte{0x12, 0x34})
+	candidates := sigs.Get(0x1234)
 	assert.Nil(candidates)
 	sigs, err = rcv.Signatures()
 	assert.NoError(err)
-	candidates = sigs.Get([]byte{0x45, 0x67})
+	candidates = sigs.Get(0x4567)
 	assert.Nil(candidates)
 }
